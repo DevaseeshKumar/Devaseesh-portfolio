800e21961c099cf504be3b7490cbb1a354fe298f
Devaseesh <devaseesh.sonti2005@gmail.com>
2025-09-07 10:31:20 +0530

    local

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // 👈 allows access from LAN
+    port: 5173,       // optional, default is 5173
+  },
 })
